021a09839d2280a0f874791dcac723ae795a2713
Hung Nguyen <hung.nguyen@sentifi.com>
2017-05-24 16:07:20 +0700

    feat UI

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './weather';
 import css from './app.scss';
 
-console.log('test');
+ReactDOM.render(
+  <App />, document.getElementById('root')
+);
---
+++ src/weather.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import HeaderUI from './components/weather-header-ui';
+
+class WeatherUI extends Component {
+  render() {
+    return (
+      <HeaderUI />
+    );
+  }
+}
+
+export default WeatherUI;
